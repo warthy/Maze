6eb9d4fc97a2f09b09b5bdc044cc304d3cb7c537
Warthy <venard.paul@gmail.com>
2019-02-28 13:52:56 +0000

    main loop game

--- main.c
+++ main.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
+#include "library\graphics.h"
 
+#include "Character.h"
 
+typedef enum { false, true } bool;
+bool isWin;
+void initGame();
 
 int main() {
 
+	initGame();
+
+	int keyPressed = 0;
+	do {
+		DrawCharacter();
+
+		if (_kbhit()) {
+			keyPressed = _getch();
+			switch (keyPressed) {
+				case KEY_UP:
+					if (1) {
+						MoveUp();
+					}
+					break;
+				case KEY_DOWN:
+					if (1) {
+						MoveDown();
+					}
+					break;
+				case KEY_RIGHT:
+					if (1) {
+						MoveRight();
+					}
+					break;
+				case KEY_LEFT:
+					if (1) {
+						MoveLeft();
+					}
+					break;
+			}
+		}
+
+	} while (isWin || keyPressed != KEY_ESCAPE);
+}
+
+
+void initGame() {
+	initCharacterPosition();
+	isWin = false;
 }
